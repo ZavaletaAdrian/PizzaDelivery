23a787c508f08417d0441c519cadd28c08ca68f5
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-05-07 14:48:41 -0500

    *Update main.cpp*

--- pizzaDelivery/main.cpp
+++ pizzaDelivery/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <iostream>
+#include<fstream>
+#include<string>
+#include <stdlib.h>
+#include "Grafo.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    Grafo *g;
+	g = new Grafo("grafo.txt");
+	g->generaGrafo("grafo.txt");
+	g->muestraListaNodos();
+	g->muestraGrafo();
+	cout << endl;
 }
